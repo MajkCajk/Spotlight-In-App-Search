83cf69dc6868dd01d38993832119f7356122e590
Michal Chobola <michal.chobola@ackee.cz>
2019-08-25 17:34:28 +0200

    add Models

---
+++ Spotlight-In-App-Search/Spotlight-In-App-Search/Model/FirstItem.swift
@@ -0,0 +1,15 @@
+//
+//  FirstItem.swift
+//  Spotlight-In-App-Search
+//
+//  Created by Michal Chobola on 25/08/2019.
+//  Copyright © 2019 MajkCajk. All rights reserved.
+//
+
+import Foundation
+
+public struct FirstItem {
+    let id: String
+    let name: String
+    let description: String
+}
