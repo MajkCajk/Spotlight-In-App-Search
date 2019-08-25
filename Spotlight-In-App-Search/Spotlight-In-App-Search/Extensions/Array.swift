1e46a35041b8b96c6ca3e93386257c31e62c5faf
Michal Chobola <michal.chobola@ackee.cz>
2019-08-25 17:33:55 +0200

    add extensions

---
+++ Spotlight-In-App-Search/Spotlight-In-App-Search/Extensions/Array.swift
@@ -0,0 +1,23 @@
+//
+//  Array.swift
+//  Spotlight-In-App-Search
+//
+//  Created by Michal Chobola on 25/08/2019.
+//  Copyright © 2019 MajkCajk. All rights reserved.
+//
+
+import Foundation
+
+/// Helpers for cleaner work with arrays
+extension Array where Element: Hashable {
+    
+    /// Will filter all duplicated elements in array.
+    ///
+    /// Used for filtering duplicated words in searched text for Spotlight search.
+    ///
+    /// source: https://stackoverflow.com/questions/27624331/unique-values-of-array-in-swift
+    func uniqued() -> [Element] {
+        var seen = Set<Element>()
+        return filter{ seen.insert($0).inserted }
+    }
+}
