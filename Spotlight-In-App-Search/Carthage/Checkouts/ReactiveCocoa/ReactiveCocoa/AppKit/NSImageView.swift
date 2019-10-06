06b33dd020bcc82504326f4c7d8171e1d459637b
michal.chobola <michal.chobola@firma.seznam.cz>
2019-10-06 17:38:14 +0200

    remove carthage checkouts from git

--- Spotlight-In-App-Search/Carthage/Checkouts/ReactiveCocoa/ReactiveCocoa/AppKit/NSImageView.swift
+++
@@ -1,9 +0,0 @@
-import ReactiveSwift
-import AppKit
-
-extension Reactive where Base: NSImageView {
-	/// Sets the currently displayed image
-	public var image: BindingTarget<NSImage?> {
-		return makeBindingTarget { $0.image = $1 }
-	}
-}
